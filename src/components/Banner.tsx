b4b55163edf444c28d873912d26733bf30ff056a
LeonardoLobas <leonardolobas2703@gmail.com>
2025-12-10 14:42:36 -0300

    Adiciona Prettier e plugin para TailwindCSS; atualiza o componente Banner e ajusta o WeatherCard

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,17 +3,17 @@ import wildbeast from "../assets/parceiros/wildbeast.svg";
 export function Banner() {
     return (
         <main className="container">
-            <div className="text-white pt-64 px-8 pb-8 bg-gradient-to-t from-verde-950/80 relative rounded-2xl overflow-hidden">
-                <img className="absolute inset-0 size-full object-cover -z-10" src={floresta} alt="Banner" />
-                <div className="bg-verde-950 inline-flex gap-8 mb-8 items-center py-1 pr-1 rounded-full pl-4">
+            <div className="from-verde-950/80 relative overflow-hidden rounded-2xl bg-gradient-to-t px-8 sm:pt-64 pb-8 text-white sm:px-4 pt-12">
+                <img className="absolute inset-0 -z-10 size-full object-cover" src={floresta} alt="Banner" />
+                <div className="sm:bg-verde-950 bg-verde-950/60 mb-8 flex flex-col items-start gap-2 rounded-xl p-4 pl-4 sm:inline-flex sm:flex-row sm:items-center sm:gap-8 sm:rounded-full sm:py-1 sm:pr-1">
                     Vagas para dezembro abertas
-                    <a className="bg-verde-400 text-verde-900 py-2 px-4 inline-block  rounded-full" href="">
+                    <a className="bg-verde-400 text-verde-900 inline-block rounded-full px-4 py-2" href="">
                         Reserve Hoje
                     </a>
                 </div>
-                <h1 className="font-serif text-balance mb-20 max-w-screen-sm text-5xl">Venha experimentar a Vida na Floresta</h1>
-                <div className="text-sm flex justify-between items-end">
-                    <p>
+                <h1 className="mb-8 sm:mb-20 max-w-screen-sm font-serif text-4xl sm:text-5xl text-balance">Venha experimentar a Vida na Floresta</h1>
+                <div className="sm:flex items-end justify-between text-sm">
+                    <p className="max-sm:mb-4">
                         Melhores locais para visitar
                         <a className="underline decoration-2 underline-offset-2 hover:no-underline" href="./">
                             2049
--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,20 +1,20 @@
 interface WeatherCardProps {
-    dia: string;
-    temperatura: string;
-    emoji: string;
+  dia: string;
+  temperatura: string;
+  emoji: string;
 }
 
 export function WeatherCard({ dia, temperatura, emoji }: WeatherCardProps) {
-    return (
-        <div className="container mt-4 flex justify-center md:justify-start">
-            <div className="inline-flex items-center rounded-md bg-verde-900">
-                <span className="size-2 bg-verde-300 block rounded-full relative -left-1"></span>
-                <div className="flex text-verde-300 divide-x-2 divide-verde-800">
-                    <div className="px-4 py-2 capitalize">{dia}</div>
-                    <div className="px-4 py-2">{temperatura}°</div>
-                    <div className="px-4 py-2">{emoji}</div>
-                </div>
-            </div>
+  return (
+    <div className="container mt-4 flex justify-center md:justify-start">
+      <div className="bg-verde-900 inline-flex items-center rounded-md">
+        <span className="bg-verde-300 relative -left-1 block size-2 rounded-full"></span>
+        <div className="text-verde-300 divide-verde-800 flex divide-x-2">
+          <div className="px-4 py-2 capitalize">{dia}</div>
+          <div className="px-4 py-2">{temperatura}°</div>
+          <div className="px-4 py-2">{emoji}</div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
