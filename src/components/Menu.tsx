4f2bdad9d25a7dc79266f3b781dc4b14a465ff20
LeonardoLobas <leonardolobas2703@gmail.com>
2026-01-05 11:49:41 -0300

    Adiciona funcionalidade de menu móvel ao componente Menu e ajusta a formatação dos links de navegação

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,35 +1,55 @@
 import logoImg from "../assets/forest.svg";
 
+const mobileButton = document.getElementById("mobile-button");
+const mobileMenu = document.getElementById("mobile-menu");
+
+mobileButton?.addEventListener("click", () => {
+    mobileMenu?.classList.toggle("hidden");
+    mobileMenu?.classList.toggle("flex");
+});
+mobileMenu?.addEventListener("click", () => {
+    mobileMenu?.classList.add("hidden");
+    mobileMenu?.classList.remove("flex");
+});
 export function Menu() {
     return (
         <header className="container items-center flex justify-between gap-8 py-8">
             <a href="./">
                 <img src={logoImg} alt="Forest" />
             </a>
-            <nav>
-                <ul className="text-white text-xl flex flex-wrap gap-8">
+            <nav id="mobile-menu" className=" lg:block z-40 hidden items-center max-lg:fixed max-lg:w-full max-lg:inset-0">
+                <div className="fixed inset-0 lg:hidden bg-verde-950/40 backdrop-blur-md"></div>
+                <ul className="text-white max-lg:z-50 max-lg:divide-y-2 max-lg:p-8 *:cursor-pointer max-lg:divide-white/10 max-lg:w-full text-2xl max-lg:absolute lg:text-xl lg:flex lg:flex-wrap lg:gap-8">
                     <li>
-                        <a className="hover:underline hover:underline-offset-8" href="#acomodacoes">
+                        <a className="lg:hover:underline max-lg:hover:bg-white/10 lg:py-2 p-4 block lg:hover:underline-offset-8" href="#acomodacoes">
                             Acomodações
                         </a>
                     </li>
                     <li>
-                        <a className="hover:underline hover:underline-offset-8" href="#eventos">
+                        <a className="lg:hover:underline max-lg:hover:bg-white/10 lg:py-2 p-4 block lg:hover:underline-offset-8" href="#eventos">
                             Eventos
                         </a>
                     </li>
                     <li>
-                        <a className="hover:underline hover:underline-offset-8" href="#experiencias">
+                        <a className="lg:hover:underline max-lg:hover:bg-white/10 lg:py-2 p-4 block lg:hover:underline-offset-8" href="#experiencias">
                             Experiências
                         </a>
                     </li>
                     <li>
-                        <a className="hover:underline hover:underline-offset-8" href="#contato">
+                        <a className="lg:hover:underline max-lg:hover:bg-white/10 lg:py-2 p-4 block lg:hover:underline-offset-8" href="#contato">
                             Contato
                         </a>
                     </li>
                 </ul>
             </nav>
+            <button id="mobile-button" className="flex items-center gap-3 cursor-pointer bg-verde-400 text-verde-900  rounded-full px-4 py-2 lg:hidden">
+                Menu{" "}
+                <span className="h-3 w-4 flex flex-col justify-between *:h-0.5 *:rounded-md *:bg-verde-800">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </span>
+            </button>
         </header>
     );
 }
